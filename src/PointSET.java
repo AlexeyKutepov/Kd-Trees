6782500209a6aa709efeda3e80a81546368412b1
Alexey Kutepov <alkutepov@gmail.com>
2015-02-28 16:00:53 +0300

    Fix of the PointSET

--- src/PointSET.java
+++ src/PointSET.java
@@ -1,44 +1,70 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 /**
  * Created by Alexey Kutepov on 28.02.15.
  */
 public class PointSET {
+    private TreeSet<Point2D> tree;
+
     // construct an empty set of points
     public PointSET() {
-
+        tree = new TreeSet<Point2D>();
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return tree.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return tree.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        tree.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        return tree.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-
+        for (Point2D point2D : tree) {
+            point2D.draw();
+        }
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (rect == null) {
+            throw new NullPointerException();
+        }
+        ArrayList<Point2D> list = new ArrayList<Point2D>();
+        for (Point2D point2D : tree) {
+            if (rect.contains(point2D)) {
+                list.add(point2D);
+            }
+        }
+        return list;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException();
+        }
+
         return null;
     }
 
